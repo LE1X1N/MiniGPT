653f45737b9287ebd29deee24fc877bf7449540c
Xin Lei <realleixin@gmail.com>
2025-12-16 21:56:18 +0800

    feat: introduce FFN

--- model/model.py
+++ model/model.py
@@ -77,6 +77,7 @@ import torch.nn as nn
 from typing import Optional
 import math
 import torch.nn.functional as F
+from transformers.activations import ACT2FN
 
 class RMSNorm(nn.Module):
     def __init__(self, dim: int, eps: float = 1e-5):
@@ -243,8 +244,18 @@ class Attention(nn.Module):
         
         
 class FeedForward(nn.Module):
-    def __init__(self, *args, **kwargs):
-        pass
-    
-    def forward():
-        pass
+    def __init__(self, args: MiniGPTConfig):
+        super().__init__()
+        if args.intermediate_size is None:
+            intermediate_size = int(args.hidden_size * 8 / 3)
+            args.intermediate_size = 64 * ((intermediate_size + 64 - 1) // 64)
+        
+        self.up_proj = nn.Linear(args.hidden_size, args.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(args.intermediate_size, args.hidden_size, bias=False)
+        self.gate_proj = nn.Linear(args.hidden_size, args.intermediate_size, bias=False)
+        self.dropout = nn.Dropout(args.dropout)
+        self.act_fn = ACT2FN[args.hidden_act]
+        
+    def forward(self, x):
+        gated = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
+        return self.dropout(self.down_proj(gated))

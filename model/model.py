6c076cf656df37320c61f8308a4f588b7df2704d
Xin Lei <realleixin@gmail.com>
2025-12-17 20:13:19 +0800

    feat: introduce MiniGPT block

--- model/model.py
+++ model/model.py
@@ -260,3 +260,38 @@ class FeedForward(nn.Module):
     def forward(self, x):
         gated = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
         return self.dropout(self.down_proj(gated))
+
+
+class MiniGPTBlock(nn.Module):
+    def __init__(self, layer_id: int, config: MiniGPTConfig):
+        super().__init__()
+        self.layer_id = layer_id
+        
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_dim = self.hidden_size // self.num_attention_heads
+        
+        self.self_attn = Attention(config)   # GQA layer
+        self.input_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps)
+        self.mlp = FeedForward(config)
+    
+    def forward(self, hidden_states: torch.Tensor,
+                positional_embeddings: torch.Tensor,
+                past_key_value: torch.Tensor = None,
+                use_cache : bool=False,
+                attention_mask: torch.Tensor = None):
+        residual = hidden_states
+        
+        hidden_states, present_key_value = self.self_attn(self.input_layernorm(hidden_states), 
+                                                          positional_embeddings,
+                                                          past_key_value,
+                                                          use_cache,
+                                                          attention_mask)
+        hidden_states = residual + hidden_states    # residual
+        
+        hidden_states = self.mlp(self.post_attention_layernorm(hidden_states)) + hidden_states
+        
+        return hidden_states, present_key_value
+        
+        

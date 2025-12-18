fix: apply frequency scaling when sequence_len larger than original max positional emb in RoPE

model/model.py
@@ -112,15 +112,17 @@ def precompute_freqs_cis(dim: int, end: int=32*1024, rope_base: float=1e6, rope_
             rope_scaling.get("beta_fast", 4),
             rope_scaling.get("beta_slow", 1),
         )
-        corr_dim = next((i for i in range(dim // 2) if 2*math.pi / freqs[i] > orig_max), dim//2)    # first rotation period that large than pre-trained orig_max  
-        power = torch.arange(0, dim//2, device=freqs.device).float() / max(dim // 2 - 1, 1)
-        beta = beta_slow + (beta_fast - beta_slow) * power
-        scale = torch.where(
-            torch.arange(dim//2, device=freqs.device) < corr_dim,
-            (beta * factor - beta + 1) / (beta * factor), 
-            1.0 / factor
-        )
-        freqs = freqs * scale
+        
+        if end > orig_max:
+            corr_dim = next((i for i in range(dim // 2) if 2*math.pi / freqs[i] > orig_max), dim//2)    # first rotation period that large than pre-trained orig_max  
+            power = torch.arange(0, dim//2, device=freqs.device).float() / max(dim // 2 - 1, 1)
+            beta = beta_slow + (beta_fast - beta_slow) * power
+            scale = torch.where(
+                torch.arange(dim//2, device=freqs.device) < corr_dim,
+                (beta * factor - beta + 1) / (beta * factor), 
+                1.0 / factor
+            )
+            freqs = freqs * scale
     
     t = torch.arange(end, device=freqs.device)
     freqs = torch.outer(t, freqs).float()   # [end, dim/2] 
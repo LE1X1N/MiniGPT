c557db677587139abfd27e8a45e105f00c393248
Xin Lei <realleixin@gmail.com>
2025-12-19 11:53:14 +0800

    feat: add pbar when loading dataset

--- dataset/lm_dataset.py
+++ dataset/lm_dataset.py
@@ -3,6 +3,7 @@ import os
 import torch
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
+from tqdm import tqdm
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -16,8 +17,13 @@ class PretrainDataset(Dataset):
     def load_data(self, path):
         samples = []
         
+        # total lines
         with open(path, "r", encoding="utf-8") as f:
-            for line in f:
+            total = sum(1 for _ in f)
+        
+        # read line
+        with open(path, "r", encoding="utf-8") as f:
+            for line in tqdm(f, total=total, desc="Loading pretrain data"):
                 data = json.loads(line.strip())
                 samples.append(data)  
         return samples

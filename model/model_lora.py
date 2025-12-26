81ec0130b4d6c76fea9eb9d24190fb88881a4ccf
Xin Lei <realleixin@gmail.com>
2025-12-26 20:02:54 +0800

    feat: add test code snippet for LoRA

--- model/model_lora.py
+++ model/model_lora.py
@@ -38,12 +38,23 @@ def apply_lora(model: nn.Module, rank: int=8):
 
 
 def save_lora(model: nn.Module, path: str):
-    state_dict = {}
+    """
+        Save A and B matrices of LoRA layers
+        
+        e.g. 
+        {
+            linear1.lora.A.weight: Tensor
+            linear1.lora.B.weight: Tensor
+            linear2.lora.A.weight: Tensor
+            linear2.lora.B.weight: Tensor
+        }
+    """
+    state_dict = {}   
     
     for name, module in model.named_modules():
         if hasattr(module, "lora"):
             clean_name = name[7:] if name.startswith("module.") else name
-            lora_state = {f"{clean_name}.lora.{k}": v for k, v in module.lora.state_dict().items()}
+            lora_state = {f"{clean_name}.lora.{k}": v for k, v in module.lora.state_dict().items()} 
             state_dict.update(lora_state)
 
     torch.save(state_dict, path)
@@ -55,9 +66,67 @@ def load_lora(model: nn.Module, path: str):
 
     for name, module in model.named_modules():
         if hasattr(module, "lora"):
+            # linear1.lora.A.weight -> A.weight
             lora_state = {k.replace(f"{name}.lora.", ""): v for k, v in state_dict.items() if f"{name}.lora." in k}
             module.lora.load_state_dict(lora_state)
 
+
+if __name__ == "__main__":
+    
+    class TestModel(nn.Module):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.linear1 = nn.Linear(128, 128)
+            self.linear2 = nn.Linear(128, 128)
+            self.linear3 = nn.Linear(128, 10)
+            
+        def forward(self, x):
+            x = self.linear1(x)
+            x = self.linear2(x)
+            x = self.linear3(x)
+            return x
+    
+    # test LoRA
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    rank = 8
+    lora_save_path = "test_lora_weights.pth"
+    
+    model = TestModel().to(device)
+    model.device = device
+    
+    # TestModel(
+    #     (linear1): Linear(in_features=128, out_features=128, bias=True)
+    #     (linear2): Linear(in_features=128, out_features=128, bias=True)
+    #     (linear3): Linear(in_features=128, out_features=10, bias=True)
+    # )
+    print("========= Model Architecture ========")
+    print(model)
+    
+    
+    # TestModel(
+    #   (linear1): Linear(
+    #     in_features=128, out_features=128, bias=True
+    #     (lora): LoRA(
+    #       (A): Linear(in_features=128, out_features=8, bias=False)
+    #       (B): Linear(in_features=8, out_features=128, bias=False)
+    #     )
+    #   )
+    #   (linear2): Linear(
+    #     in_features=128, out_features=128, bias=True
+    #     (lora): LoRA(
+    #       (A): Linear(in_features=128, out_features=8, bias=False)
+    #       (B): Linear(in_features=8, out_features=128, bias=False)
+    #     )
+    #   )
+    #   (linear3): Linear(in_features=128, out_features=10, bias=True)
+    # )
+    print("========= Model Architecture (LoRA) ========")
+    apply_lora(model, rank)
+    print(model)
+
+    # save and load
+    save_lora(model, lora_save_path)
+    load_lora(model, lora_save_path)
            
 
     

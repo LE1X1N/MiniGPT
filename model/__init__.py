0d2ec2e7d9525c6bfe8b3a70f2411cb4a2bbe998
Xin Lei <realleixin@gmail.com>
2026-01-06 11:19:47 +0800

    feat: update model init

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,8 @@
 from .model import MiniGPTForCausalLM, MiniGPTConfig
+from .model_lora import apply_lora, load_lora, save_lora
 
-__all__ = ["MiniGPTForCausalLM", "MiniGPTConfig"]
+__all__ = ["MiniGPTForCausalLM", 
+           "MiniGPTConfig", 
+           "apply_lora", 
+           "load_lora",
+           "save_lora"]
